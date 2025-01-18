35251d4ff1fc0d27538f66a6ce2a7333cbb7d84d
Pius Prince <piusprince61@gmail.com>
2025-01-18 15:12:26 +0000

    feat: Add Sanity client and image URL builder utility

---
+++ src/lib/sanity.ts
@@ -0,0 +1,15 @@
+import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = createClient({
+  apiVersion: "2022-03-07",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  useCdn: false,
+});
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: string) {
+  return builder.image(source);
+}
