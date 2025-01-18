90e86b49358bec1f2bc93515661cd01ba4d864ab
Pius Prince <piusprince61@gmail.com>
2025-01-18 15:13:52 +0000

    feat: Add tech configuration for various technologies with icons

---
+++ src/lib/tech.ts
@@ -0,0 +1,12 @@
+import { Icons } from "@/components/ui/icons";
+
+export const tech = {
+  react: { name: "React", icon: Icons.React },
+  next: { name: "Next.js", icon: Icons.Nextjs },
+  typescript: { name: "TypeScript", icon: Icons.Typescript },
+  tailwind: { name: "Tailwind CSS", icon: Icons.TailwindCSS },
+  sanity: { name: "Sanity", icon: Icons.Sanity },
+  radix: { name: "Radix", icon: Icons.RadixUI },
+  //   graphql: { name: "GraphQL", icon: Icons.Graphql },
+  //   apollo: { name: "Apollo", icon: Icons.Apollo },
+};
