3a811e7ed2012ff4132e0ea93e3701708b0b3e8e
Pius Prince <piusprince61@gmail.com>
2025-02-07 20:38:43 +0000

    feat: Add Sanity API read token and enable stega configuration in client

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -6,6 +6,11 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: false,
+  token: process.env.SANITY_API_READ_TOKEN,
+  stega: {
+    enabled: true,
+    studioUrl: process.env.NEXT_PUBLIC_SANITY_STUDIO_URL,
+  },
 });
 
 const builder = imageUrlBuilder(client);
